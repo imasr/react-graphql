2c422927a02b17cb72f2baf7f55d8f0953af8184
imasr <ashishs723@gmail.com>
2019-11-15 21:14:20 +0530

    form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,73 +2,101 @@ import React from 'react';
 import './Login.scss';
 import Logo from '../assets/app_icon.png';
 
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField'
 
 export default class Login extends React.Component {
-    constructor() {
-        super()
-        this.submitForm = (email, password) => {
-            console.log(email, password)
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: { email: '', password: '' },
+            error: {}
         }
-        this.changeEmail = (ev) => {
-            console.log(ev.target.value)
+    }
+
+    handleValidation = event => {
+        let formObj = this.state.form
+    }
+
+    submitForm = event => {
+        console.log(this.state)
+        if (this.handleValidation()) {
         }
-        this.changePassword = (ev) => {
-            console.log(ev.target.value)
-        }
+        event.preventDefault();
+    }
+    changefield = (ev) => {
+        let form = this.state.form
+        form[ev.target.name] = ev.target.value
+        this.setState({ form });
     }
 
 
     render() {
-        const email = this.props.email;
-        const password = this.props.password;
         return (
             <div className="container">
                 <div className="Login">
                     <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12 maxWidth p-4">
                         <div className="">
-                            <img src={Logo} className="logo img rounded" width="60" alt="" />
+                            <img
+                                src={Logo}
+                                className="logo img rounded"
+                                width="60"
+                                alt="" />
                         </div>
                         <h5 className="py-2">Sign In</h5>
-                        <form className="needs-validation" onSubmit={this.submitForm(email, password)}>
-                            <fieldset className="form-group">
-                                <input
-                                    className="form-control font-size-14px  rounded-0"
+                        <form className="needs-validation" onSubmit={this.submitForm}>
+                            <div>
+                                <TextField
+                                    label="Email"
+                                    margin="normal"
+                                    variant="outlined"
+                                    className="font-size-14px fullWidth rounded-0"
                                     type="email"
+                                    name='email'
                                     placeholder="Email"
-                                    value={email}
-                                    onChange={this.changeEmail} />
-                            </fieldset>
+                                    onChange={this.changefield}
+                                />
+                            </div>
 
-                            <fieldset className="form-group">
-                                <input
-                                    className="form-control font-size-14px  rounded-0"
+                            <div >
+                                <TextField
+                                    id="outlined-basic"
+                                    label="Password"
+                                    margin="normal"
+                                    variant="outlined"
+                                    className="font-size-14px fullWidth rounded-0"
                                     type="password"
+                                    name='password'
                                     placeholder="Password"
-                                    value={password}
-                                    onChange={this.changePassword} />
-                            </fieldset>
-                            <fieldset className="text-right">
-
-                                <button className="btn btn-primary rounded-0 font-size-14px"
-                                    type="submit"> Sign in
-                                </button>
-                            </fieldset>
+                                    onChange={this.changefield}
+                                />
+                            </div>
+                            <div
+                                className="text-right">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    className="btnLogin font-size-14px"
+                                    width={1}
+                                    type="submit"
+                                >Sign in</Button>
+                            </div>
                         </form>
-                        <div class="font-size-14px">
+                        <div className="font-size-14px">
                             <a href="/forgot" >Forgot password?</a>
                         </div>
-                        <div class="py-2 font-size-14px">
+                        <div className="py-2 font-size-14px">
                             <a href="/new" >New User?</a>
                         </div>
-                        <div class="d-flex">
-                            <a href="/fb" class="fb text-white rounded-circle mr-1 p-2 pointer" data-toggle="tooltip" data-placement="top"
+                        <div className="py-2 font-size-14px">
+                            <a href="/fb" className="fb text-white mr-1 p-2 pointer" data-toggle="tooltip" data-placement="top"
                                 title="Facebook login" >
-                                <i class="fa fa-facebook fa-fw font-size-18px"></i>
+                                <i className="fa fa-facebook fa-fw font-size-18px"></i>
                             </a>
 
-                            <a href="/google" class="google rounded-circle text-white p-2 pointer" data-toggle="tooltip"
+                            <a href="/google" className="google text-white p-2 pointer" data-toggle="tooltip"
                                 data-placement="top" title="Google Login" id="googleBtn">
-                                <i class="fa fa-google fa-fw font-size-18px">
+                                <i className="fa fa-google fa-fw font-size-18px">
                                 </i>
                             </a>
                         </div>
